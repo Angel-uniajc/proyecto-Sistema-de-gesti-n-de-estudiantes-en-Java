0429cded46179c7d9bc83eeed90c1b2461afbc59
Joel-404 <joelmina046@gmail.com>
2026-09-09 14:00:25 -0500

    creamos un metodo para guaradar los datos en el txt

--- Archivos.java
+++ Archivos.java
@@ -1,7 +1,20 @@
+import java.io.FileWriter;
 
 public class Archivos {
     
-    private static final String name_archivo = "estudiantes.txt";  
+    private static final String name_archivo = "estudiantes.txt";  // Nombre del archivo
+
+    // Método para guardar los datos en el archivo
+    public static void guardarEstudiantes(ArrayList<Estudiante> estudiantes) {
+
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(name_archivo))){ //creamos un metodo para guardar los datos en el archivo
+
+           
+            
+               
+
+    
+        }
 
     
 
