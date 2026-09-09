0dc02e21a7ae14ff717f363e7366998577faaefd
SebashoDev <sebastianmartinez@uniajc.edu.co>
2026-09-09 00:54:07 -0500

    Se creó la clase GestionEstudiante.java y se declaró un arraylist para almacenar los estudiantes.

---
+++ GestionEstudiantes.java
@@ -0,0 +1,8 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class GestionEstudiantes {
+    //ArrayList para almacenar los estudiantes
+    private ArrayList<Estudiante> estudiantes;
+
+}
