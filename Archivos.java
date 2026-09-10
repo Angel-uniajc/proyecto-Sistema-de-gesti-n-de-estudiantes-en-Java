83b9afb453fc50284e95ab5813bbc07497f25083
Joel-404 <joelmina046@gmail.com>
2026-09-09 20:32:27 -0500

    se añadio un arralist para poder obtener las notas

--- Archivos.java
+++ Archivos.java
@@ -1,4 +1,5 @@
 import java.io.FileWriter;
+import java.util.ArrayList;
 
 public class Archivos {
     
@@ -8,14 +9,30 @@ public class Archivos {
     public static void guardarEstudiantes(ArrayList<Estudiante> estudiantes) {
 
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(name_archivo))){ //creamos un metodo para guardar los datos en el archivo
-
-           
             
-               
+            for (Estudiante estudiante : estudiantes) { //recorremos la lista de estudiantes
+
+                writer.write( //escribimos cada estudiante en el archivo
+                    estudiante.getId() + ";" + 
+                    estudiante.getNombre() + ";" +
+                    estudiante.getApellido() + ";"
+                );
+           
+            ArrayList<Double> notas = estudiante.getNotas(); //obtenemos las notas del estudiante
+                for ( int i = 0; i < notas.size(); i++){//recorrer  las notas
+                    writer.write(String.valueOf(notas.get(i))); // para escribir cada nota
+
+                    if( i < notas.size()-1){
+                        writer.write(",");
+                    }
+                }
+               writer.newLine(); //para escribir una nueva linea
 
     
         }
 
     
 
+}
+}
 }
